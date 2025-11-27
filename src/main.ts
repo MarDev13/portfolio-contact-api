05f0f211d1b263d044e12055eb20e701999dc2e4
MarDev13 <mardevvdesign@gmail.com>
2025-11-27 23:06:12 +0100

    config: setup CORS and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:4321/', 'https://marmartinezdev.netlify.app/'],
+    methods: 'POST,OPTIONS',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+});
