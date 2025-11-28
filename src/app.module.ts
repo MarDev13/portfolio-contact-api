95d2106c7ad0d7212a18eac8dc1fb851362558d3
MarDev13 <mardevvdesign@gmail.com>
2025-11-28 13:17:48 +0100

    chore(config): enable global ConfigModule for .env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ContactModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
